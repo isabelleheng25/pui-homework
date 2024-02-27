c2af7dc41de7938e37d45805867d491194a4e93a
izzyheng <iheng@andrew.cmu.edu>
2024-02-26 21:01:26 -0500

    fixed hw 5

--- solution-hw5/js/remove.js
+++ solution-hw5/js/remove.js
@@ -1,18 +1,35 @@
 const rollSet = new Set();
 
+let glaze = {
+    "Original": 0,
+    "Sugar Milk": 0,
+    "Vanilla Milk": 0.5,
+    "Double Chocolate": 1.5,
+
+};
+
+let pack = {
+    "1": 1,
+    "3": 3,
+    "6": 5,
+    "12": 10,
+}
+
 class Roll {
-    constructor(rollImage, rollType, rollGlazing, packSize, calculatedPrice) {
+    constructor(rollImage, rollType, rollGlazing, packSize, basePrice) {
         this.image = rollImage;
         this.type = rollType;
         this.glazing = rollGlazing;
         this.size = packSize;
-        this.calculatedPrice = calculatedPrice;
+        this.basePrice = basePrice;
+
+        this.calculatedPrice = (this.basePrice + glaze[this.glazing]) * pack[this.size];
     }
   }
 
-function addRoll(rollImage, rollType, rollGlazing, packSize, calculatedPrice) {
+function addRoll(rollImage, rollType, rollGlazing, packSize, basePrice) {
 
-    const roll = new Roll(rollImage, rollType, rollGlazing, packSize, calculatedPrice);
+    const roll = new Roll(rollImage, rollType, rollGlazing, packSize, basePrice);
     rollSet.add(roll);
     return roll;
 }
@@ -21,7 +38,7 @@ function addRoll(rollImage, rollType, rollGlazing, packSize, calculatedPrice) {
 function updateTotalPrice() {
     let totalPrice = 0;
     rollSet.forEach(roll => {
-        totalPrice += parseFloat(roll.calculatedPrice.replace('$', ''));
+        totalPrice += parseFloat(roll.calculatedPrice);
     });
     document.getElementById('total-price').textContent = '$' + totalPrice.toFixed(2);
 }
@@ -31,32 +48,32 @@ const originalRoll = addRoll(
     "assets/products/original-cinnamon-roll.jpg",
     "Original Cinnamon Roll",
     "Sugar Milk",
-    "Pack Size: 1",
-    "$2.49"
+    1,
+    rolls["Original"]["basePrice"]
 );
 
 const walnutRoll = addRoll(
     "assets/products/walnut-cinnamon-roll.jpg",
     "Walnut Cinnamon Roll",
     "Vanilla Milk",
-    "Pack Size: 12",
-    "$39.90"
+    12,
+    rolls["Walnut"]["basePrice"]
 );
 
 const raisinRoll = addRoll(
     "assets/products/raisin-cinnamon-roll.jpg",
     "Raisin Cinnamon Roll",
     "Sugar Milk",
-    "Pack Size: 3",
-    "$8.97"
+    3,
+    rolls["Raisin"]["basePrice"]
 );
 
 const appleRoll = addRoll(
     "assets/products/apple-cinnamon-roll.jpg",
     "Apple Cinnamon Roll",
     "Original",
-    "Pack Size: 3",
-    "$10.47"
+    3,
+    rolls["Apple"]["basePrice"]
 );
 
 for (const roll of rollSet) {
@@ -91,8 +108,8 @@ function updateElement(roll) {
     rollImageElement.src = roll.image;
     rollTitleElement.innerText = roll.type;
     rollGlazeElement.innerText = roll.glazing;
-    rollPackElement.innerText = roll.size;
-    rollPriceElement.innerText = roll.calculatedPrice;
+    rollPackElement.innerText = "Pack Size: " + roll.size;
+    rollPriceElement.innerText = roll.calculatedPrice.toFixed(2);
 }
 
 function deleteNote(roll) {
